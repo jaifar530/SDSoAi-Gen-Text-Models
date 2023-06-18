b576153bf6f2cb1158c734c631a7ca935bd43018
jaifar530 <31308766+jaifar530@users.noreply.github.com>
2023-06-18 21:01:45 +0100

    added the model

--- app.py
+++ app.py
@@ -6,3 +6,120 @@ st.title("Smart Detection System of AI-Generated Text Models")
 #subtitle 
 st.markdown("## This is a POC repo for Smart Detection System of AI Generated Text Models project, it is a pre-trained model that detect the probablities of using any of the known LLM (chatgpt3, chatgpt4, GoogleBard, HuggingfaceChat)##")
 
+import os
+import requests
+import pickle
+import pandas as pd
+import nltk
+import spacy
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+import numpy as np
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+
+# Check if the file exists
+if not os.path.isfile('RandomForestClassifier.pkl'):
+    # Download the zip file if it doesn't exist
+    url = 'https://jaifar.net/RandomForestClassifier.pkl'
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    }
+
+    response = requests.get(url, headers=headers)
+
+    # Save the file
+    with open('RandomForestClassifier.pkl', 'wb') as file:
+        file.write(response.content)
+
+# At this point, the pickle file should exist, either it was already there, or it has been downloaded and extracted.
+with open('RandomForestClassifier.pkl', 'rb') as file:
+    clf_loaded = pickle.load(file)
+
+
+
+# Loading a SpaCy model for Named Entity Recognition and Lemmatization
+nlp = spacy.load('en_core_web_sm')
+
+# # Your input paragraph
+# input_paragraph = "Your paragraph here..."
+
+# # Read the paragraph from a text file
+# with open('paragraph.txt', 'r') as file:
+#     input_paragraph = file.read()
+
+input_paragraph = st.text_area("Input your text here")
+
+df = pd.DataFrame(columns=["paragraph"])
+df = df.append({"paragraph": input_paragraph}, ignore_index=True)
+
+
+
+# Variable to control number of words to retrieve
+num_words = 500
+
+# Retrieving only the first num_words words of the paragraph
+input_paragraph = ' '.join(word_tokenize(input_paragraph)[:num_words])
+
+# Extracting features
+def extract_features(text):
+    words = word_tokenize(text)
+    sentences = sent_tokenize(text)
+    doc = nlp(text)
+
+    avg_word_length = sum(len(word) for word in words) / len(words)
+    avg_sent_length = sum(len(sent) for sent in sentences) / len(sentences)
+    punctuation_count = len([char for char in text if char in '.,;:?!'])
+    stopword_count = len([word for word in words if word in stopwords.words('english')])
+    lemma_count = len(set(token.lemma_ for token in doc))
+    named_entity_count = len(doc.ents)
+
+    tagged_words = nltk.pos_tag(words)
+    pos_counts = nltk.FreqDist(tag for (word, tag) in tagged_words)
+    pos_features = {
+        'pos_IN': pos_counts['IN'],
+        'pos_DT': pos_counts['DT'],
+        'pos_NN': pos_counts['NN'],
+        'pos_,': pos_counts[','],
+        'pos_VBZ': pos_counts['VBZ'],
+        'pos_WDT': pos_counts['WDT'],
+        'pos_TO': pos_counts['TO'],
+        'pos_VB': pos_counts['VB'],
+        'pos_VBG': pos_counts['VBG'],
+        'pos_.': pos_counts['.'],
+        'pos_JJ': pos_counts['JJ'],
+        'pos_NNS': pos_counts['NNS'],
+        'pos_RB': pos_counts['RB'],
+        'pos_CC': pos_counts['CC'],
+        'pos_VBN': pos_counts['VBN'],
+    }
+
+    features = {
+        'avg_word_length': avg_word_length,
+        'avg_sent_length': avg_sent_length,
+        'punctuation_count': punctuation_count,
+        'stopword_count': stopword_count,
+        'lemma_count': lemma_count,
+        'named_entity_count': named_entity_count,
+    }
+    features.update(pos_features)
+
+    return pd.Series(features)
+    #return pd.DataFrame(features)
+
+
+# Creates a button named 'Press me'
+press_me_button = st.button("Press me")
+
+if press_me_button:
+    # Display the text entered by the user
+    
+    input_features = df['paragraph'].apply(extract_features)
+    predicted_llm = clf_loaded.predict(input_features)
+    st.write(f"Predicted LLM: {predicted_llm[0]}")
+
+# Get the features of the input paragraph
+#input_features = extract_features(input_paragraph)
+
+
