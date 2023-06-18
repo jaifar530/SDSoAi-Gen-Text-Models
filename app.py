03d9d998c6fc92c6908c347b4aad73415c16540c
jaifar530 <31308766+jaifar530@users.noreply.github.com>
2023-06-18 19:56:54 +0100

    Added demo app

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+#title 
+st.title("Smart Detection System of AI-Generated Text Models")
+
+#subtitle 
+st.markdown("## This is a POC repo for Smart Detection System of AI Generated Text Models project, it is a pre-trained model that detect the probablities of using any of the known LLM (chatgpt3, chatgpt4, GoogleBard, HuggingfaceChat)##")
+
