60e08853ee1aa116d5014918823989fa046844ea
jaifar530 <31308766+jaifar530@users.noreply.github.com>
2023-09-17 21:21:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -101,47 +101,26 @@ for filename, url in file_urls.items():
     #     st.write(f"File {filename} already exists. Skipping download.")
 ############ download ridge and ExtraTree stuff
 
+def has_internet_connection():
+    try:
+        response = requests.get("https://www.google.com/", timeout=5)
+        return True
+    except requests.ConnectionError:
+        return False
+
+def is_zip_file(file_path):
+    return zipfile.is_zipfile(file_path)
+
+def are_files_extracted(extracted_files, missing_files):
+    for file in missing_files:
+        if file not in extracted_files:
+            return False
+    return True
 
 def check_and_download_files():
-    file_names = [
-        "truncated_260_to_284.xlsx_vectorizer.pkl",
-        "not_trancated_full_paragraph.xlsx_extra_trees_model.pkl",
-        "not_trancated_full_paragraph.xlsx_ridge_model.pkl",
-        "not_trancated_full_paragraph.xlsx_vectorizer.pkl",
-        "truncated_10_to_34.xlsx_extra_trees_model.pkl",
-        "truncated_10_to_34.xlsx_ridge_model.pkl",
-        "truncated_10_to_34.xlsx_vectorizer.pkl",
-        "truncated_35_to_59.xlsx_extra_trees_model.pkl",
-        "truncated_35_to_59.xlsx_ridge_model.pkl",
-        "truncated_35_to_59.xlsx_vectorizer.pkl",
-        "truncated_60_to_84.xlsx_extra_trees_model.pkl",
-        "truncated_60_to_84.xlsx_ridge_model.pkl",
-        "truncated_60_to_84.xlsx_vectorizer.pkl",
-        "truncated_85_to_109.xlsx_extra_trees_model.pkl",
-        "truncated_85_to_109.xlsx_ridge_model.pkl",
-        "truncated_85_to_109.xlsx_vectorizer.pkl",
-        "truncated_110_to_134.xlsx_extra_trees_model.pkl",
-        "truncated_110_to_134.xlsx_ridge_model.pkl",
-        "truncated_110_to_134.xlsx_vectorizer.pkl",
-        "truncated_135_to_159.xlsx_extra_trees_model.pkl",
-        "truncated_135_to_159.xlsx_ridge_model.pkl",
-        "truncated_135_to_159.xlsx_vectorizer.pkl",
-        "truncated_160_to_184.xlsx_extra_trees_model.pkl",
-        "truncated_160_to_184.xlsx_ridge_model.pkl",
-        "truncated_160_to_184.xlsx_vectorizer.pkl",
-        "truncated_185_to_209.xlsx_extra_trees_model.pkl",
-        "truncated_185_to_209.xlsx_ridge_model.pkl",
-        "truncated_185_to_209.xlsx_vectorizer.pkl",
-        "truncated_210_to_234.xlsx_extra_trees_model.pkl",
-        "truncated_210_to_234.xlsx_ridge_model.pkl",
-        "truncated_210_to_234.xlsx_vectorizer.pkl",
-        "truncated_235_to_259.xlsx_extra_trees_model.pkl",
-        "truncated_235_to_259.xlsx_ridge_model.pkl",
-        "truncated_235_to_259.xlsx_vectorizer.pkl",
-        "truncated_260_to_284.xlsx_extra_trees_model.pkl",
-        "truncated_260_to_284.xlsx_ridge_model.pkl"
-    ]
+    file_names = [ ... ]  # Existing list of files
     missing_files = []
+
     for file_name in file_names:
         if not os.path.exists(file_name):
             missing_files.append(file_name)
@@ -150,28 +129,39 @@ def check_and_download_files():
         print("The following files are missing:")
         for file_name in missing_files:
             print(file_name)
-
+        
+        if not has_internet_connection():
+            print("No internet connection. Cannot download missing files.")
+            return
+        
         try:
             headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
             }
-            url = 'https://jaifar.net/ADS/content.zip'  # Replace with the actual URL
-
+            url = 'https://jaifar.net/ADS/content.zip'
             response = requests.get(url, headers=headers)
             response.raise_for_status()
 
             with open('content.zip', 'wb') as zip_file:
                 zip_file.write(response.content)
 
+            if not is_zip_file('content.zip'):
+                print("Downloaded content is not a ZIP file.")
+                return
+
             with zipfile.ZipFile('content.zip', 'r') as zip_ref:
                 zip_ref.extractall()
 
+            extracted_files = os.listdir()
+            if not are_files_extracted(extracted_files, missing_files):
+                print("Not all missing files were extracted.")
+                return
+            
             print("content.zip downloaded and extracted successfully.")
         except Exception as e:
             print(f"Error downloading or extracting content.zip: {e}")
     else:
         print("All files exist.")
-
 
 check_and_download_files()
 
