d38cd8722dedd3899f60da760e2816d7b2b94dc3
jaifar530 <31308766+jaifar530@users.noreply.github.com>
2023-09-18 12:53:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,45 @@ import numpy as np
 from PIL import Image
 from joblib import load
 import math
-from streamlit_extras.let_it_rain import rain
+import streamlit as st
+
+st.title("Rain Emojis")
+
+# Custom HTML and JavaScript for falling emojis
+st.markdown(
+    """
+    <style>
+    .emoji {
+        position: fixed;
+        font-size: 54px;
+    }
+    </style>
+    <div id="emoji-container"></div>
+    <script>
+    function makeEmojiFall() {
+        var emoji = document.createElement("div");
+        emoji.className = "emoji";
+        emoji.innerText = "🎈";
+        document.getElementById("emoji-container").appendChild(emoji);
+        emoji.style.left = Math.random() * window.innerWidth + "px";
+        emoji.style.animationDuration = Math.random() * 5 + 3 + "s";
+        emoji.style.animationName = "fall";
+    }
+    setInterval(makeEmojiFall, 1000);
+    </script>
+    <style>
+    @keyframes fall {
+        from {
+            top: -100px;
+        }
+        to {
+            top: 100vh;
+        }
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
 
 
@@ -325,12 +363,12 @@ if press_me_button:
     if ridge_prediction == extra_trees_prediction == predicted_author:
         st.success(f"Most likely written by: **{ridge_name}**", icon="✅")
         st.info("We are quite confident in the accuracy of this result.", icon="ℹ️")
-        rain(
-            emoji="😃",
-            font_size=54,
-            falling_speed=5,
-            animation_length="infinite",
-        )
+        # rain(
+        #     emoji="😃",
+        #     font_size=54,
+        #     falling_speed=5,
+        #     animation_length="infinite",
+        # )
 
     else:
         # Repeat the text with a space at the end of each iteration
@@ -380,48 +418,48 @@ if press_me_button:
             st.success(f"Most likely written by: **{ridge_name}**", icon="✅")
             st.warning(f"**Notice:** Your input text has been magnified {amplify} times to better capture its characteristics and patterns.", icon="⚠️")
             st.write("_" * 30)
-            rain(
-                emoji="😃",
-                font_size=54,
-                falling_speed=5,
-                animation_length="infinite",
-            )
+            # rain(
+            #     emoji="😃",
+            #     font_size=54,
+            #     falling_speed=5,
+            #     animation_length="infinite",
+            # )
             
         elif ridge_prediction == extra_trees_prediction:
             st.success(f"Most likely written by: **{ridge_name}**", icon="✅")
             st.success(f"2nd Most likely written by: **{cnn_name}**", icon="✅")
             st.warning(f"**Notice:** The input text has been magnified {amplify} times to better capture its characteristics and patterns.", icon="⚠️")
             st.write("_" * 30)
-            rain(
-                emoji="😐",
-                font_size=54,
-                falling_speed=5,
-                animation_length="infinite",
-            )
+            # rain(
+            #     emoji="😐",
+            #     font_size=54,
+            #     falling_speed=5,
+            #     animation_length="infinite",
+            # )
             
         elif extra_trees_prediction == predicted_author:
             st.success(f"Most likely written by: **{extra_trees_name}**", icon="✅")
             st.success(f"2nd Most likely written by: **{ridge_name}**", icon="✅")
             st.warning(f"**Notice:** The input text has been magnified {amplify} times to better capture its characteristics and patterns.", icon="⚠️")
             st.write("_" * 30)
-            rain(
-                emoji="😐",
-                font_size=54,
-                falling_speed=5,
-                animation_length="infinite",
-            )
+            # rain(
+            #     emoji="😐",
+            #     font_size=54,
+            #     falling_speed=5,
+            #     animation_length="infinite",
+            # )
             
         elif ridge_prediction == predicted_author:
             st.success(f"Most likely written by: **{ridge_name}**", icon="✅")
             st.success(f"2nd Most likely written by: **{extra_trees_name}**", icon="✅")
             st.warning(f"**Notice:** The input text has been magnified {amplify} times to better capture its characteristics and patterns.", icon="⚠️")
             st.write("_" * 30)
-            rain(
-                emoji="😐",
-                font_size=54,
-                falling_speed=5,
-                animation_length="infinite",
-            )
+            # rain(
+            #     emoji="😐",
+            #     font_size=54,
+            #     falling_speed=5,
+            #     animation_length="infinite",
+            # )
 
             
         else:
@@ -431,12 +469,12 @@ if press_me_button:
             st.success(f"3- **{extra_trees_name}**", icon="✅")
             st.warning(f"**Notice 2:** The input text has been magnified {amplify} times to better capture its characteristics and patterns.", icon="⚠️")
             st.write("_" * 30)
-            rain(
-                emoji="😕",
-                font_size=54,
-                falling_speed=5,
-                animation_length="infinite",
-            )
+            # rain(
+            #     emoji="😕",
+            #     font_size=54,
+            #     falling_speed=5,
+            #     animation_length="infinite",
+            # )
 
 
         # with st.expander("What is this project about?"):
