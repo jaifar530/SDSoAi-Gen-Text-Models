f9798ee3e1a0f6f00adb98f2bb237de328ad7d04
jaifar530 <31308766+jaifar530@users.noreply.github.com>
2023-09-20 08:44:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -355,7 +355,7 @@ if press_me_button:
         else:
             amplify = math.ceil(max_word_count / word_count)
         
-        for _ in range(3):
+        for _ in range(2):
             repeated_text += new_text + " "
 
         new_text = repeated_text
